33b5c00f1f07a279753fbb52f5b44d551977fa19
NasrWahi <nasr.wahi@gmail.com>
2026-04-08 14:03:05 +0200

    eda...

--- src/data/eda_duckdb.py
+++ src/data/eda_duckdb.py
@@ -1,4 +1,11 @@
-import eda_duckdb
+import duckdb
 import pandas as pd
 
 con = duckdb.connect(database=':memory:')
+
+con.execute("""
+    CREATE OR REPLACE VIEW bostader AS
+    SELECT * FROM read_json_auto('src/data/bostader.json')
+""")
+
+print
