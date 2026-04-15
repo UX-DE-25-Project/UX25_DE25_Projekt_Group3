e7fc73d3abf9acf81783933a957cb6ccacb55ca9
NasrWahi <nasr.wahi@gmail.com>
2026-04-15 23:04:46 +0200

    to update etl_pipeline, so that the main.py works

--- main.py
+++ main.py
@@ -10,18 +10,26 @@ def main():
     
     # STEG 1: EXTRACT
     df_raw = extract("src/data/bostader.json")
+    # Hämta SCB-datan
+    with open("src/data/scb_stats.json", "r", encoding="utf-8") as f:
+        scb_stats = json.load(f)
+    
     print(f"   Antal rader inlästa: {len(df_raw)}")
     print(f"   Kolumner: {list(df_raw.columns)}\n")
     
     #  STEG 2: TRANSFORM (+ SCB-data)
-    df_enriched = transform(df_raw, stats)
+    df_enriched = transform(df_raw, scb_stats)
+    # Spara den nya datan lokalt innan Load till Supabase
+    df_enriched.to_json("src/data/bostader_rich.json", orient="records", force_ascii=False, indent=4)
     
-    #  STEG 3: LOAD till Supabase (efter Supabase är uppsatt)
-    # load(df_clean)
-    
-    print("Extract + transform klar! Nästa steg: Load")
+    print("Extract + transform klar! Filen 'bostader_rich.json' skapad.")
+    print("Nästa steg: Load till Supabase (när det är uppsatt).")
 
 if __name__ == "__main__":
     main()
+
+        #  STEG 3: LOAD till Supabase (efter Supabase är uppsatt)
+    # load(df_clean)
+
 
 
