19b68ec171947b546dc59471a659d08584272215
NasrWahi <nasr.wahi@gmail.com>
2026-05-03 22:01:30 +0200

    render filter bar & alt

--- streamlit_app/components/filter_bar.py
+++ streamlit_app/components/filter_bar.py
@@ -5,3 +5,14 @@
 
 import streamlit as st
 from utils.helpers import load_all, get_omraden
+
+
+def render_filter_bar() -> tuple:
+    """
+    Renderar sökfält och filteralternativ.
+
+    Returns:
+        tuple: (filtrerat DataFrame, valt område som sträng)
+    """
+    df = load_all()
+    omraden = get_omraden(df)
